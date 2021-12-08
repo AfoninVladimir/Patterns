b75ae92d6ac1662a77b535d388aabcfc741cab32
AfoninVladimir <vova.afonin.2001@mail.ru>
2021-12-08 11:17:01 +0400

    commit#1
    Pattern Composite

---
+++ Composite/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Client client = new Client();
+
+        // Таким образом, клиентский код может поддерживать простые
+        // компоненты-листья...
+        Leaf leaf = new Leaf();
+        Console.WriteLine("Клиент: Я получаю простой компонент:");
+        client.ClientCode(leaf);
+        // ...а также сложные контейнеры.
+
+        Composite box = new Composite();
+        Composite branch1 = new Composite();
+        Composite branch2 = new Composite();
+        branch1.Add(new Leaf());
+        branch1.Add(new Leaf());
+        branch2.Add(new Leaf());
+        box.Add(branch1);
+        box.Add(branch2);
+
+        Console.WriteLine("Клиент: Теперь у меня есть составное дерево:");
+        client.ClientCode(box);
+
+
+        Console.Write("Клиент: Мне не нужно проверять классы компонентов даже при управлении деревом:\n");
+        client.ClientCode2(box, leaf);
+
+        }
+    }
